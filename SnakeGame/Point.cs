3ecbebe64b5f48a9d629b5b926b9fa8510ce07f4
Anastasiia Vydysh <anastasiiavydysh@gmail.com>
2022-12-11 22:50:12 +0200

    Add project files.

---
+++ SnakeGame/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    public class Point
+    {
+        int left, top;
+        char symbol;
+
+        public Point(int left, int top, char symbol)
+        {
+            this.left = left;
+            this.top = top;
+            this.symbol = symbol;
+        }
+
+        public void DrawPoint()
+        {
+            Console.SetCursorPosition(left, top);
+            Console.Write(symbol);
+        }
+    }
+}
---
+++ SnakeGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace SnakeGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Point point = new Point(5,5,'*');
+            point.DrawPoint();
+        }
+    }
+}
